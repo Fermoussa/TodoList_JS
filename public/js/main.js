a72e70f82952c93b1682a05e2038547dedb62809
moussaFer <feraouni.moussa.info@gmail.com>
2023-12-05 11:17:16 +0100

    presque fini exo

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ let btnAjouter = document.getElementById("boutonAjouter");
 
 
 
-btnAjouter.addEventListener("click",function(){
+btnAjouter.addEventListener("click", function () {
 
     let firstInput = document.getElementById("premierInput");
     let valFirstInput = firstInput.value;
@@ -16,9 +16,10 @@ btnAjouter.addEventListener("click",function(){
     paragraphe.appendChild(txtParagraphe);
     div.appendChild(paragraphe);
 
-
     div.style.backgroundColor = "rgb(240,128,128)";
     div.style.border = "2px solid red";
+    div.style.color = "white";
+    div.style.padding = "5px";
     div.style.width = "550px";
     div.style.height = "150px";
     div.style.marginLeft = "400px";
@@ -33,7 +34,7 @@ btnAjouter.addEventListener("click",function(){
 
     let contentBoutons = document.createElement("div");
     contentBoutons.style.display = "flex";
-    contentBoutons.style.backgroundColor = "orange";
+    contentBoutons.style.backgroundColor = "rgb(39,83,165)";
     contentBoutons.style.flexDirection = "column";
     div.appendChild(contentBoutons);
 
@@ -42,12 +43,12 @@ btnAjouter.addEventListener("click",function(){
     boutonFini.appendChild(txtBoutonFini);
     contentBoutons.appendChild(boutonFini);
 
-    boutonFini.addEventListener("click",function(){
+    boutonFini.addEventListener("click", function () {
         div.style.backgroundColor = "lightgreen";
         div.style.border = "2px solid green";
         boutonFini.innerHTML = "Fini";
 
-        boutonFini.addEventListener("dblclick",function(){
+        boutonFini.addEventListener("dblclick", function () {
             div.style.backgroundColor = "rgb(240,128,128)";
             div.style.border = "2px solid red";
             boutonFini.innerHTML = "Finir";
@@ -61,7 +62,7 @@ btnAjouter.addEventListener("click",function(){
     boutonModif.appendChild(txtBoutonModif);
     contentBoutons.appendChild(boutonModif);
 
-    boutonModif.addEventListener("click",function(){
+    boutonModif.addEventListener("click", function () {
         let nvlValeur = document.createElement("input");
         contentBoutons.insertBefore(nvlValeur, boutonModif);
 
@@ -71,14 +72,14 @@ btnAjouter.addEventListener("click",function(){
         boutonModif.replaceWith(validModif);
         contentBoutons.insertBefore(validModif, boutonSupp);
 
-        validModif.addEventListener("click",function(){
+        validModif.addEventListener("click", function () {
             // valFirstInput = nvlValeur.value;
 
             paragraphe.innerHTML = nvlValeur.value;
             console.log(`ANCIENNE VALEUR: ${paragraphe.innerHTML}`);
 
             console.log(`NOUVELLE VALEUR: ${paragraphe.innerHTML}`);
-            
+
             // let test = document.createTextNode(nvlValeur.value);
             // txt.replaceWith(test);
 
@@ -88,24 +89,24 @@ btnAjouter.addEventListener("click",function(){
 
     })
 
-   
 
-     // ! -------------------------------------------------
+
+    // ! -------------------------------------------------
 
     let boutonSupp = document.createElement("button");
     let txtBoutonSupp = document.createTextNode("Supprimer");
     boutonSupp.appendChild(txtBoutonSupp);
     contentBoutons.appendChild(boutonSupp);
 
-    boutonSupp.addEventListener("click",function(){
-        
+    boutonSupp.addEventListener("click", function () {
+
         let confirmSupp = document.createElement("button");
         let txtConfirmSupp = document.createTextNode("Confirmez supression");
         confirmSupp.appendChild(txtConfirmSupp);
         contentBoutons.appendChild(confirmSupp);
         boutonSupp.replaceWith(confirmSupp);
 
-        confirmSupp.addEventListener("click",function(){
+        confirmSupp.addEventListener("click", function () {
             div.remove();
         })
         // * -----------------------------------------
@@ -114,11 +115,41 @@ btnAjouter.addEventListener("click",function(){
         annulerSupp.appendChild(txtAnnuleSupp);
         contentBoutons.appendChild(annulerSupp);
 
-        annulerSupp.addEventListener("click",function(){
+        annulerSupp.addEventListener("click", function () {
             confirmSupp.replaceWith(boutonSupp);
             annulerSupp.remove();
         })
     })
 
+    // ! -------------------------------------------------------
+
+    let btnVoirTout = document.getElementById("boutonTous");
+    btnVoirTout.addEventListener("click", function () {
+        div.style.display = "flex";
+    })
+
+    let btnVoirEnCours = document.getElementById("boutonEnCours");
+    btnVoirEnCours.addEventListener("click",function(){
+        if(div.style.border == "2px solid red"){
+            div.style.display = "flex";
+        }
+        else{
+            div.style.display = "none";
+        }
+    })
+
+    let btnVoirFini = document.getElementById("boutonFini");
+    btnVoirFini.addEventListener("click",function(){
+        if(div.style.border != "2px solid red"){
+            div.style.display = "flex";
+        }
+        else{
+            div.style.display = "none";
+        }
+    })
+
+    
+
 
 })
+
